622fa9ab9063b51611e770f47625dfe95c5cdce5
Sergio Casas <sdcasas@gmail.com>
2016-02-04 12:31:56 -0300

    modificando el orden de los campos en el modelo agentes

--- models/db_03_rrhh.py
+++ models/db_03_rrhh.py
@@ -8,12 +8,12 @@ Areas = db.define_table('areas'
 Agentes = db.define_table('agentes'
     , Field('apellido')
     , Field('nombres')
+    , Field('area_id', Areas)
     , Field('domicilio')
     , Field('dni')
     , Field('tel_coorporativo')
     , Field('tel_celular')
     , Field('tel_fijo')
-    , Field('area_id', Areas)
     , auth.signature
     , format='%(apellido)s %(nombres)s'
     # , migrate=True
