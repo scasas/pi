8ab22951efdecb5d7c9c71ae006fbe64ea549f05
Sergio Casas <sdcasas@gmail.com>
2016-02-19 12:34:51 -0300

    haciendo pruebas con los reportes (reportlab)

---
+++ controllers/consultas.py
@@ -0,0 +1,34 @@
+@auth.requires_login()
+def equipos():
+    # response.view='generadores/listado.html'
+    response.title = ' Listado Completo '
+    response.subtitle = ' - app PI'
+    return dict(tipo='listado')
+
+@auth.requires_login()
+def get_data():
+
+    query="""
+        select * from pc
+        """ 
+
+    # if (request.vars['tipo'] == 'patogenicos'):
+    #     query += " WHERE generadores.tipo_generador<>1 "
+
+    # if (request.vars['tipo'] == 'peligrosos'):
+    #     query += " WHERE generadores.tipo_generador=1 "
+
+    query += """
+
+                """ 
+
+
+    iTotalRecords='select count(id) from pc;'
+    iTotalDisplayRecords = iTotalRecords
+    custdata = {}
+    custdata['aaData'] = db.executesql(query)
+    custdata['iTotalRecords']=db.executesql(iTotalRecords)[0][0]
+    custdata['iTotalDisplayRecords']=db.executesql(iTotalDisplayRecords)[0][0]
+    custdata['sEcho']=1
+
+    return response.json(custdata)
